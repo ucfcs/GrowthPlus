61bf9960fea1bc29affcef89f92619307741a77f
Leticia Barbosa <leticia.vrealb@gmail.com>
2022-10-28 14:30:21 -0400

    Removed 'score' from Lessons, put vcs in .gitignore

--- app/src/main/java/com/fall2022_group20/dataAccessLayer/Lesson/LessonSchema.java
+++ app/src/main/java/com/fall2022_group20/dataAccessLayer/Lesson/LessonSchema.java
@@ -8,8 +8,6 @@ public class LessonSchema extends RealmObject{
     @PrimaryKey
     private Integer id;
 
-    //took this from the diagram, how are points and score different?
-    private Integer score;
     private Integer points;
 
     private String lessonName;
@@ -19,13 +17,6 @@ public class LessonSchema extends RealmObject{
     }
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    public Integer getScore() {
-        return score;
-    }
-    public void setScore(Integer score) {
-        this.score = score;
     }
 
     public Integer getPoints() {return points;}
