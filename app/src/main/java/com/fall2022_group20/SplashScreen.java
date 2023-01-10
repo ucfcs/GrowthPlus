4833fe00538e5e3b1309c6e271afdf9f08a3b23b
RuthShryock <ruthieshryock@gmail.com>
2023-01-09 20:51:32 -0500

    inital launch screen code, added font family

---
+++ app/src/main/java/com/fall2022_group20/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.fall2022_group20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent view = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(view);
+                finish();
+
+            }
+        }, 4000);
+    }
+}
