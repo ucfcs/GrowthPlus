4ff5de7e4d009f124d2e07b77f1d74fcc62869ed
Julio Alva <julioalva1992@gmail.com>
2023-01-12 17:52:11 -0500

    added background, back button to roadmap one

--- app/src/main/java/com/fall2022_group20/MainActivity.java
+++ app/src/main/java/com/fall2022_group20/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_roadmap_one);
         realm = Realm.getDefaultInstance();
     }
 }
