736d8bfb980979661eef0f79f44bb2430bb31e73
RuthShryock <ruthieshryock@gmail.com>
2023-01-13 10:45:28 -0500

    inital child portal code

---
+++ app/src/main/java/com/fall2022_group20/ChildPortal.java
@@ -0,0 +1,14 @@
+package com.fall2022_group20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChildPortal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child_portal);
+    }
+}
