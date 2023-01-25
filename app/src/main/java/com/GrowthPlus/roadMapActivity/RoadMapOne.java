708e60537862a3e36d635197fa054fa54340d566
Julio Alva <julioalva1992@gmail.com>
2023-01-25 17:16:08 -0500

    integrated roadmap screens

---
+++ app/src/main/java/com/GrowthPlus/roadMapActivity/RoadMapOne.java
@@ -0,0 +1,73 @@
+package com.GrowthPlus.roadMapActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.GrowthPlus.R;
+import com.GrowthPlus.customViews.RoadMapLessonTrail;
+import com.GrowthPlus.customViews.TopBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class RoadMapOne extends AppCompatActivity {
+
+    ImageButton goBackButton;
+    BottomNavigationView bottomNavigationView;
+    RoadMapLessonTrail roadMapOneLessonTrail;
+    ConstraintLayout roadMapOne;
+    TopBar topBarOne;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_road_map_one);
+
+        init();
+
+        roadMapOneLessonTrail.unLockRoadMap();
+        roadMapOneLessonTrail.setSelectedState(roadMapOneLessonTrail.getRoadMapTile1());
+
+        bottomNavigationView.setSelectedItemId(R.id.roadMap1item);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.roadMap1item:
+                    return true;
+                case R.id.roadMap2item:
+                    startActivity(new Intent(getApplicationContext(), RoadMapTwo.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.roadMap3item:
+                    startActivity(new Intent(getApplicationContext(), RoadMapThree.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.roadMap4item:
+                    startActivity(new Intent(getApplicationContext(), RoadMapFour.class));
+                    overridePendingTransition(0,0);
+                    return true;
+            }
+            return false;
+        });
+
+        goBackButton.setOnClickListener(v -> onBackPressed());
+
+    }
+
+    private void init(){
+
+        bottomNavigationView = findViewById(R.id.roadMapBottomNavigationView);
+        roadMapOneLessonTrail = findViewById(R.id.roadMapOneLessonTrail);
+        roadMapOne = findViewById(R.id.roadMapOne);
+        goBackButton = roadMapOne.findViewById(R.id.goBackBtn);
+        topBarOne = findViewById(R.id.topBarOne);
+    }
+
+    private void lockedState(){
+        roadMapOneLessonTrail.setAlpha(.7f);
+    }
+}
---
+++ app/src/main/java/com/GrowthPlus/roadMapActivity/RoadMapTwo.java
@@ -0,0 +1,70 @@
+package com.GrowthPlus.roadMapActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import com.GrowthPlus.R;
+import com.GrowthPlus.customViews.RoadMapLessonTrail;
+import com.GrowthPlus.customViews.TopBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class RoadMapTwo extends AppCompatActivity {
+
+    ImageButton goBackButton;
+    BottomNavigationView bottomNavigationView;
+    ConstraintLayout roadMapTwo;
+    RoadMapLessonTrail roadMapTwoLessonTrail;
+    TopBar topBarTwo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_road_map_two);
+
+        init();
+        lockedState();
+
+        bottomNavigationView.setSelectedItemId(R.id.roadMap2item);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.roadMap1item:
+                    startActivity(new Intent(getApplicationContext(), RoadMapOne.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.roadMap2item:
+                    return true;
+                case R.id.roadMap3item:
+                    startActivity(new Intent(getApplicationContext(), RoadMapThree.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.roadMap4item:
+                    startActivity(new Intent(getApplicationContext(), RoadMapFour.class));
+                    overridePendingTransition(0,0);
+                    return true;
+            }
+            return false;
+        });
+
+        goBackButton.setOnClickListener(v -> onBackPressed());
+    }
+
+    private void init(){
+
+        bottomNavigationView = findViewById(R.id.roadMapBottomNavigationView);
+        roadMapTwo = findViewById(R.id.roadMapTwo);
+        topBarTwo = roadMapTwo.findViewById(R.id.topBarTwo);
+        goBackButton = topBarTwo.findViewById(R.id.goBackBtn);
+        roadMapTwoLessonTrail = roadMapTwo.findViewById(R.id.roadMapTwoLessonTrail);
+
+    }
+
+    private void lockedState(){
+        roadMapTwoLessonTrail.setAlpha(.7f);
+    }
+}
