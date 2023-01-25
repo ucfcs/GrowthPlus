2abe574ebba50ca3059e2aa0387c3abadcbc3215
Julio Alva <julioalva1992@gmail.com>
2023-01-25 16:11:08 -0500

    updated custom views

---
+++ app/src/main/java/com/GrowthPlus/customViews/TopBar.java
@@ -0,0 +1,49 @@
+package com.GrowthPlus.customViews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.GrowthPlus.R;
+
+public class TopBar extends ConstraintLayout {
+
+    ImageButton goBackButton;
+    ImageView pointsIcon;
+    TextView points;
+
+    public TopBar(@NonNull Context context) {
+        super(context);
+        init(null);
+    }
+
+    public TopBar(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public TopBar(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    public TopBar(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs);
+    }
+
+    private void init(@Nullable AttributeSet attrs) {
+        inflate(getContext(), R.layout.activity_top_bar, this);
+        goBackButton = findViewById(R.id.goBackBtn);
+        pointsIcon = findViewById(R.id.pointsIcon);
+        points = findViewById(R.id.points);
+    }
+
+
+}
