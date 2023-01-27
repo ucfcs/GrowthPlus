991e50f01471a76648858087766ffe3404a1b9a5
Julio Alva <[julioalva1992@gmail.com]>
2023-01-27 11:45:28 -0500

    select child account screen

--- app/src/main/java/com/GrowthPlus/SelectChildAvatar.java
+++ app/src/main/java/com/GrowthPlus/SelectChildAvatar.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CreateAccount extends AppCompatActivity {
+public class SelectChildAvatar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_account);
+        setContentView(R.layout.activity_select_child_avatar);
     }
 }
