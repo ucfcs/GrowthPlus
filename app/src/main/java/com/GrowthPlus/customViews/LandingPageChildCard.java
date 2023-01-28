f7c62d931a7a5885d70f2caf0b628ae57d031e06
Julio Alva <[julioalva1992@gmail.com]>
2023-01-28 14:58:09 -0500

    landing page child card component

--- app/src/main/java/com/GrowthPlus/customViews/LandingPageChildCard.java
+++ app/src/main/java/com/GrowthPlus/customViews/LandingPageChildCard.java
@@ -1,38 +1,56 @@
 package com.GrowthPlus.customViews;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.GrowthPlus.R;
 
+import java.lang.ref.Reference;
+
 public class LandingPageChildCard extends ConstraintLayout {
 
-    private ChildAvatarComponent landingPageChildAvatar;
+    private ImageView landingPageChildAvatar;
     private TextView landingPageChildName;
+    private String childName;
+    private ColorStateList nameBackground;
+    private ColorStateList avatarBackgroundColor;
+    private int avatarSrc;
+
 
     public LandingPageChildCard(@NonNull Context context) {
         super(context);
         init(null);
+        getCustomAttributes(context, null, 0);
     }
 
     public LandingPageChildCard(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init(attrs);
+        getCustomAttributes(context, attrs, 0);
     }
 
     public LandingPageChildCard(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init(attrs);
+        getCustomAttributes(context, attrs, defStyleAttr);
     }
 
     public LandingPageChildCard(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         init(attrs);
+        getCustomAttributes(context, attrs, defStyleAttr);
     }
 
     private void init(@Nullable AttributeSet set){
@@ -40,4 +58,43 @@ public class LandingPageChildCard extends ConstraintLayout {
         landingPageChildAvatar = findViewById(R.id.landingPageChildAvatar);
         landingPageChildName = findViewById(R.id.landingPageChildName);
     }
+
+
+    private void getCustomAttributes(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+
+        TypedArray typedArray = null;
+        if (attrs != null) {
+
+            typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.LandingPageChildCard, defStyleAttr, 0);
+            childName = typedArray.getString(R.styleable.LandingPageChildCard_childName);
+            nameBackground = typedArray.getColorStateList(R.styleable.LandingPageChildCard_nameBackgroundColor);
+            avatarBackgroundColor = typedArray.getColorStateList(R.styleable.LandingPageChildCard_avatarBackgroundColor);
+            avatarSrc = typedArray.getResourceId(R.styleable.LandingPageChildCard_avatarSrc, R.mipmap.bunny_foreground);
+        }
+
+        setChildCardName(childName);
+        setNameBackground(nameBackground);
+        setAvatarBackgroundColor(avatarBackgroundColor);
+        setAvatarSrc(avatarSrc);
+
+      typedArray.recycle();
+    }
+
+    public void setChildCardName(String name){
+
+        landingPageChildName.setText(name);
+    }
+
+    public void setNameBackground (ColorStateList color){
+        landingPageChildName.setBackgroundTintList(color);
+    }
+
+    public void setAvatarBackgroundColor(ColorStateList color){
+        landingPageChildAvatar.setBackgroundTintList(color);
+    }
+
+    public void setAvatarSrc(int imageResource){
+        landingPageChildAvatar.setImageResource(imageResource);
+    }
+
 }
