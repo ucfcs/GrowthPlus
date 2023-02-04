bd95facae66b950f8428cd4596ca13f0d9cf38d4
Mack Orooji <mackorooji8@gmail.com>
2023-02-04 18:00:50 -0500

    small changes, nothing to take note of

--- app/src/main/java/com/GrowthPlus/ParentLogin.java
+++ app/src/main/java/com/GrowthPlus/ParentLogin.java
@@ -2,13 +2,25 @@ package com.GrowthPlus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ParentLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_parent_login);
+
+
+        Button backButton = (Button) findViewById(R.id.backParentLogin);
+        backButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(ParentLogin.this, ParentSignup.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/GrowthPlus/ParentSignup.java
+++ app/src/main/java/com/GrowthPlus/ParentSignup.java
@@ -2,7 +2,13 @@ package com.GrowthPlus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.GrowthPlus.dataAccessLayer.parent.ParentSchema;
 
 public class ParentSignup extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class ParentSignup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_signup);
+
+        Button signupButton = (Button) findViewById(R.id.parentLoginBtn);
+        signupButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(ParentSignup.this, ParentLogin.class));
+            }
+        });
+
+        Button parentSUBackButton = (Button) findViewById(R.id.backParentSU);
+        parentSUBackButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(ParentSignup.this, MainActivity.class));
+            }
+        });
     }
 }
