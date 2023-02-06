323bbc2e7f3888a567b73ac58cf02e435cc56074
Leticia Barbosa <leticia.vrealb@gmail.com>
2023-02-06 17:19:17 -0500

    Completed Leaderboard logic, some bugs

--- app/src/main/java/com/GrowthPlus/Leaderboard.java
+++ app/src/main/java/com/GrowthPlus/Leaderboard.java
@@ -1,36 +1,79 @@
 package com.GrowthPlus;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
+
 import com.GrowthPlus.dataAccessLayer.child.ChildSchema;
 import com.GrowthPlus.dataAccessLayer.child.ChildSchemaService;
+import com.GrowthPlus.realmImporter.JsonSampleData;
 
-import io.realm.Realm;
-import io.realm.RealmList;
+import java.util.HashMap;
+
 import io.realm.RealmResults;
+import io.realm.gradle.Realm;
 
-public class Leaderboard extends AppCompatActivity {
+public class Leaderboard extends AppCompatActivity implements View.OnClickListener {
 
+    Realm realm;
+    Resources resources;
+
+    private Button backChildPortalBtn;
+    private GridLayout leaderBoardGridLayout;
+
+    private ChildSchemaService childrenLeaderBoard;
+
+    private HashMap<Integer, Integer> leaderBoardChildCardIds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
 
-        Button backSet = (Button) findViewById(R.id.backChildPortal);
+        init();
+        importSampleData();
 
-        backSet.setOnClickListener(new View.OnClickListener(){
+        RealmResults<ChildSchema> children = childrenLeaderBoard.getAllChildSchemas();
+
+        backChildPortalBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 startActivity(new Intent(Leaderboard.this, MainActivity.class));
             }
         });
 
+    }
+
+    private void init(){
+        realm = Realm.getDefaultInstance();
+
+        backChildPortalBtn = findViewById(R.id.backChildPortal);
+        leaderBoardGridLayout = findViewById(R.id.leaderBoardGridLayout);
+        childrenLeaderBoard = new ChildSchemaService(realm);
+        leaderBoardChildCardIds = new HashMap<>();
+
+    }
+
+    private void importSampleData(){
+        JsonSampleData jsonSampleData = new JsonSampleData(realm, resources);
+        jsonSampleData.importDataFromJson();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    private void setChildCardIds(){
+        leaderBoardChildCardIds.put(0, R.id.leaderBoardChildCard0);
+        leaderBoardChildCardIds.put(1, R.id.leaderBoardChildCard1);
+        leaderBoardChildCardIds.put(2, R.id.leaderBoardChildCard2);
+        leaderBoardChildCardIds.put(3, R.id.leaderBoardChildCard3);
+        leaderBoardChildCardIds.put(4, R.id.leaderBoardChildCard4);
+        leaderBoardChildCardIds.put(5, R.id.leaderBoardChildCard5);
     }
 }
--- app/src/main/java/com/GrowthPlus/customViews/LeaderboardChildView.java
+++ app/src/main/java/com/GrowthPlus/customViews/LeaderboardChildView.java
@@ -3,13 +3,13 @@ package com.GrowthPlus.customViews;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.ContextCompat;
 
 import com.GrowthPlus.R;
 
@@ -18,6 +18,7 @@ public class LeaderboardChildView extends ConstraintLayout{
     private ImageView childAvatar;
     private TextView childName;
     private TextView childScore;
+    private View nameBlock;
 
     public LeaderboardChildView(@NonNull Context context) {
         super(context);
@@ -44,6 +45,29 @@ public class LeaderboardChildView extends ConstraintLayout{
         childAvatar = findViewById(R.id.leaderboardAvatarImg);
         childName = findViewById(R.id.leaderboardName);
         childScore = findViewById(R.id.leaderboardPoints);
+        nameBlock = findViewById(R.id.nameBlock);
+
+        /*ColorStateList color = ContextCompat.getColorStateList(getContext(), R.color.red);
+        setBackgroundTintList(color);
+        setText("Billie");
+        setPointsText("65");
+        setImageResource(R.mipmap.camel_foreground);*/
     }
 
+    //childAvatar is ImgView with the avatar img to be changed
+    public void setImageResource (int resId){
+        childAvatar.setImageResource(resId);
+    }
+    //childName is the TextView with the name that needs to be changed
+    public void setText (CharSequence newName){
+        childName.setText(newName);
+    }
+    //nameBlock is a View contains the way to change the entire background of the card
+    public void setBackgroundTintList (ColorStateList tint){
+        nameBlock.setBackgroundTintList(tint);
+    }
+    //childScore is a TextView that contains text that needs to be changed
+    public void setPointsText (CharSequence points){
+        childScore.setText(points);
+    }
 }
