a1568f50cd176c043da1cd55bc3030f9622c2e91
Victor Yue <89821054+VictorYue@users.noreply.github.com>
2023-02-08 14:54:24 -0500

    Created all fragments.

---
+++ app/src/main/java/com/GrowthPlus/fragment/ShapesAngles1.java
@@ -0,0 +1,25 @@
+package com.GrowthPlus.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.GrowthPlus.R;
+public class ShapesAngles1 extends Fragment {
+    View view;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_shapes_angles1, container, false);
+        return view;
+    }
+}
