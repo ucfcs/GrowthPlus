c3e1fff9214d02c90fa3bdecfe5874ced1aa2159
RuthShryock <ruthieshryock@gmail.com>
2023-02-11 10:36:56 -0500

    refactoring flashcard shema and cleaning up shared preferences

--- app/src/main/java/com/GrowthPlus/LanguageSettingActivity.java
+++ app/src/main/java/com/GrowthPlus/LanguageSettingActivity.java
@@ -2,25 +2,18 @@ package com.GrowthPlus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.GrowthPlus.dataAccessLayer.Language.LanguageSchema;
 import com.GrowthPlus.dataAccessLayer.Language.LanguageSchemaService;
-import com.GrowthPlus.realmImporter.LanguagesRealmImporter;
-
-
-import java.io.InputStream;
 
 import io.realm.Realm;
 
@@ -122,10 +115,6 @@ public class LanguageSettingActivity extends AppCompatActivity implements View.O
         // create instance of shared preferences
         SharedPreferences langPrefs = getSharedPreferences("LangPreferences", MODE_PRIVATE);
         String langId = langPrefs.getString("languageId", "frenchZero");
-        // import language json file
-        InputStream langInputStream = resources.openRawResource(R.raw.languages);
-        LanguagesRealmImporter langRealmImporter = new LanguagesRealmImporter(realm, resources, langInputStream);
-        langRealmImporter.importLanguagesFromJson();
         // create language schema service and set strings
         LanguageSchemaService langSchemaService = new LanguageSchemaService(realm, langPrefs.getString("languageId", langId));
         LanguageSchema lang = langSchemaService.getLanguageSchemaById();
