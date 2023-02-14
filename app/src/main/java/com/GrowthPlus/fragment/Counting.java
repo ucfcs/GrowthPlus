3e2d07b55b964f50082d8f0c791c865f78a0f130
Julio Alva <[julioalva1992@gmail.com]>
2023-02-14 14:37:14 -0500

    implemented correct num of images per content

--- app/src/main/java/com/GrowthPlus/fragment/Counting.java
+++ app/src/main/java/com/GrowthPlus/fragment/Counting.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.gridlayout.widget.GridLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.GrowthPlus.R;
@@ -18,7 +20,7 @@ public class Counting extends Fragment {
     View view;
     TextView word;
     TextView number;
-    ImageView img;
+    GridLayout gridLayoutCounting;
     ImageSrcIdentifier imageSrcIdentifier;
 
     @Override
@@ -26,7 +28,7 @@ public class Counting extends Fragment {
         view = inflater.inflate(R.layout.fragment_counting, container, false);
         word = view.findViewById(R.id.wordCounting);
         number = view.findViewById(R.id.numberCounting);
-        img = view.findViewById(R.id.imageCounting);
+        gridLayoutCounting = view.findViewById(R.id.gridLayoutCounting);
         imageSrcIdentifier = new ImageSrcIdentifier();
         return view;
     }
@@ -40,9 +42,27 @@ public class Counting extends Fragment {
         String getWord = requireArguments().getString("lessonWord");
         String getNumber = requireArguments().getString("lessonNumber");
         String getImg = requireArguments().getString("lessonImage");
+        int imgNum = Integer.parseInt(getNumber);
 
         word.setText(getWord);
         number.setText(getNumber);
-        img.setImageResource(imageSrcIdentifier.getImageSrcId(getImg));
+
+
+        int resId = imageSrcIdentifier.getImageSrcId(getImg);
+        for(int i=0; i<imgNum; i++){
+            ImageView imageTemp = setImageView(resId, 70, 70);
+            gridLayoutCounting.addView(imageTemp, i);
+        }
+
+    }
+
+    public ImageView setImageView(int resId, int width, int height){
+
+        ImageView imageTemp = new ImageView(getActivity());
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(width, height);
+        imageTemp.setLayoutParams(layoutParams);
+        imageTemp.setImageResource(resId);
+
+        return imageTemp;
     }
 }
