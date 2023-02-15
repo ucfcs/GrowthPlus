6201f3285be588c27fb4d3759437c98538d11d61
Julio Alva <julioalva1992@gmail.com>
2023-02-15 15:47:48 -0500

    added fragments to support flashcards

---
+++ app/src/main/java/com/GrowthPlus/fragment/CustomImageOperator.java
@@ -0,0 +1,28 @@
+package com.GrowthPlus.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.GrowthPlus.R;
+
+public class CustomImageOperator extends Fragment {
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_custom_image_operator, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+    }
+}
