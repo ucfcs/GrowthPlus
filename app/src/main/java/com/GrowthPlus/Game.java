b74a274a8ef2621cd0047f0474d412f90a7c94b2
Mack Orooji <mackorooji8@gmail.com>
2023-02-20 10:02:29 -0500

    Figured out why the triangle was getting cut off when I made it larger - I needed to make the layout_width and height larger as well.

--- app/src/main/java/com/GrowthPlus/Game.java
+++ app/src/main/java/com/GrowthPlus/Game.java
@@ -19,7 +19,7 @@ public class Game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-A        init();
+        init();
         setTopBar();
     }
 
