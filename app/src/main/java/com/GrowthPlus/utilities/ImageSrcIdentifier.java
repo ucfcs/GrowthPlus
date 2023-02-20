26ad69ce22e5e2771f7f85c3d9e40e473e546a62
RuthShryock <ruthieshryock@gmail.com>
2023-02-20 15:36:24 -0500

    adding the rest of level 2 lesson content

--- app/src/main/java/com/GrowthPlus/utilities/ImageSrcIdentifier.java
+++ app/src/main/java/com/GrowthPlus/utilities/ImageSrcIdentifier.java
@@ -30,6 +30,18 @@ public class ImageSrcIdentifier {
         imageSrcId.put("twentyFiveFrancs", R.mipmap.twenty_five_fcfa_foreground);
         imageSrcId.put("fiftyFrancs", R.mipmap.fifty_fcfa_foreground);
         imageSrcId.put("oneHundredFrancs", R.mipmap.one_hundred_fcfa_foreground);
+        imageSrcId.put("centimeter", R.mipmap.centimeter_foreground);
+        imageSrcId.put("decimeter", R.mipmap.decimeter_foreground);
+        imageSrcId.put("meter", R.mipmap.meter_foreground);
+        imageSrcId.put("hectometer", R.mipmap.hectometer_foreground);
+        imageSrcId.put("centigram", R.mipmap.centigram_foreground);
+        imageSrcId.put("decigram", R.mipmap.decigram_foreground);
+        imageSrcId.put("gram", R.mipmap.gram_foreground);
+        imageSrcId.put("decagram", R.mipmap.decagram_foreground);
+        imageSrcId.put("hectogram", R.mipmap.hectogram_foreground);
+        imageSrcId.put("centiliter", R.mipmap.centiliter_foreground);
+        imageSrcId.put("deciliter", R.mipmap.deciliter_foreground);
+        imageSrcId.put("liter", R.mipmap.liter_foreground);
     }
 
     public Integer getImageSrcId(String imageName){
