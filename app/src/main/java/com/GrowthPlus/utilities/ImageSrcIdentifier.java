c2c23d018c6314cf7deb637efb42e82bd5241b13
RuthShryock <ruthieshryock@gmail.com>
2023-02-22 00:10:02 -0500

    added up to lesson 7 of level 3

--- app/src/main/java/com/GrowthPlus/utilities/ImageSrcIdentifier.java
+++ app/src/main/java/com/GrowthPlus/utilities/ImageSrcIdentifier.java
@@ -30,18 +30,33 @@ public class ImageSrcIdentifier {
         imageSrcId.put("twentyFiveFrancs", R.mipmap.twenty_five_fcfa_foreground);
         imageSrcId.put("fiftyFrancs", R.mipmap.fifty_fcfa_foreground);
         imageSrcId.put("oneHundredFrancs", R.mipmap.one_hundred_fcfa_foreground);
+        imageSrcId.put("fiveHundredCoin", R.mipmap.five_hundred_coin_fcfa_foreground);
         imageSrcId.put("centimeter", R.mipmap.centimeter_foreground);
         imageSrcId.put("decimeter", R.mipmap.decimeter_foreground);
         imageSrcId.put("meter", R.mipmap.meter_foreground);
         imageSrcId.put("hectometer", R.mipmap.hectometer_foreground);
+        imageSrcId.put("millimeter", R.mipmap.millimeter_foreground);
+        imageSrcId.put("kilometer", R.mipmap.kilometer_foreground);
         imageSrcId.put("centigram", R.mipmap.centigram_foreground);
         imageSrcId.put("decigram", R.mipmap.decigram_foreground);
         imageSrcId.put("gram", R.mipmap.gram_foreground);
         imageSrcId.put("decagram", R.mipmap.decagram_foreground);
         imageSrcId.put("hectogram", R.mipmap.hectogram_foreground);
+        imageSrcId.put("milligram", R.mipmap.milligram_foreground);
+        imageSrcId.put("kilogram", R.mipmap.kilogram_foreground);
         imageSrcId.put("centiliter", R.mipmap.centiliter_foreground);
         imageSrcId.put("deciliter", R.mipmap.deciliter_foreground);
         imageSrcId.put("liter", R.mipmap.liter_foreground);
+        imageSrcId.put("milliliter", R.mipmap.milliter_foreground);
+        imageSrcId.put("kiloliter", R.mipmap.kiloliter_foreground);
+        imageSrcId.put("fiveHundredFrancs", R.mipmap.five_hundred_fcfa_foreground);
+        imageSrcId.put("oneThousandFrancs", R.mipmap.one_thousand_fcfa_foreground);
+        imageSrcId.put("clock", R.mipmap.clock_foreground);
+        imageSrcId.put("circle", R.mipmap.circle_foreground);
+        imageSrcId.put("rectangle", R.mipmap.rectangle_foreground);
+        imageSrcId.put("triangleYellow", R.mipmap.triangle_foreground);
+        imageSrcId.put("triangleGreen", R.mipmap.triangle_green_foreground);
+
     }
 
     public Integer getImageSrcId(String imageName){
