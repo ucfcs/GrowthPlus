9f97a46019539892bf7890a0d12f825a0574fd09
Julio Alva <[julioalva1992@gmail.com]>
2023-03-02 12:39:44 -0500

    clean up

--- app/src/main/java/com/GrowthPlus/MainActivity.java
+++ app/src/main/java/com/GrowthPlus/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         init();
         importRoadMapData();
-        importChildData();
+        //importChildData(); UNCOMMENT FOR TESTING
         setAllLandingPageCards(children);
 
         parentPortal.setOnClickListener(this);
